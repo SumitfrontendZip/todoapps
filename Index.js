21cee3562fd8a7838540ff849a81410bdfc8706b
Sumit Kumar <sumit@frontendzip.com>
2024-05-01 18:39:09 +0530

    i am inserting enter hitting system and fix UI in taskbar

--- Index.js
+++ Index.js
@@ -1,55 +1,62 @@
 // creating a input section add a todo list
 
-
 const inputsection = document.getElementById("inputsection");
 const imgBtn = document.createElement("img");
-imgBtn.src = "arrow.png"
-inputsection.append(imgBtn)
+imgBtn.src = "arrow.png";
+inputsection.append(imgBtn);
 
+const input = document.createElement("input");
+input.placeholder = "What needs to be done";
+inputsection.append(input);
 
-const input = document.createElement("input")
-input.placeholder = "What needs to be done"
-inputsection.append(input)
+// create a task bar
+const taskBar = document.createElement("div");
+taskBar.classList.add("taskbar")
 
-// create a task bar 
+input.addEventListener("keypress", () => {
+  if (event.key == "Enter") {
 
-const taskSection = document.getElementById("taskSection");
-const checkBtn = document.createElement("img");
-checkBtn.src ="check.png"
-taskSection.append(checkBtn)
+    const taskSection = document.getElementById("taskSection");
 
-const taskTitle = document.createElement("span");
-taskTitle.innerText = "Go to gym"
-taskSection.append(taskTitle)
+    const value = input.value;
 
+    const checkBtn = document.createElement("img");
+    checkBtn.src = "check.png";
+    taskSection.append(checkBtn);
 
-const taskcross = document.createElement("img");
-taskcross.src = "cross.png"
-taskSection.append(taskcross)
+    const taskTitle = document.createElement("span");
+    taskTitle.innerText = value;
+    taskSection.append(taskTitle);
 
+    const taskcross = document.createElement("img");
+    taskcross.src = "cross.png";
+    taskSection.append(taskcross);
+
+    taskBar.append(taskSection)
+  }
+});
 
 // create a down bar buttons
 
 const buttonSection = document.getElementById("buttonSection");
 
-const remainText = document.createElement("span")
-remainText.innerText = "2 items left"
-buttonSection.append(remainText)
+const remainText = document.createElement("span");
+remainText.innerText = "2 items left";
+buttonSection.append(remainText);
 
 const buttons = document.createElement("div");
-buttonSection.append(buttons)
+buttonSection.append(buttons);
 
-const allBtn = document.createElement("span")
-allBtn.innerText = "All"
-buttons.append(allBtn)
-const activeBtn = document.createElement("span")
-activeBtn.innerText = "Active"
-buttons.append(activeBtn)
-const completeBtn = document.createElement("span")
-completeBtn.innerText = "Complete"
-buttons.append(completeBtn)
-
+const allBtn = document.createElement("span");
+allBtn.innerText = "All";
+buttons.append(allBtn);
+const activeBtn = document.createElement("span");
+activeBtn.innerText = "Active";
+buttons.append(activeBtn);
+const completeBtn = document.createElement("span");
+completeBtn.innerText = "Complete";
+buttons.append(completeBtn);
 
 const clearBtn = document.createElement("span");
-clearBtn.innerText = "Clear"
-buttonSection.append(clearBtn)
+clearBtn.innerText = "Clear";
+buttonSection.append(clearBtn);
