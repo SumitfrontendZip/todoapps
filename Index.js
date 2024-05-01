811cfc4aa54229d7003578e18f1892560976ef78
Sumit Kumar <sumit@frontendzip.com>
2024-05-01 14:52:00 +0530

    correction in css and create a tasks bar

--- Index.js
+++ Index.js
@@ -10,3 +10,21 @@ inputsection.append(imgBtn)
 const input = document.createElement("input")
 input.placeholder = "What needs to be done"
 inputsection.append(input)
+
+// create a task bar 
+
+const taskSection = document.getElementById("taskSection");
+const checkBtn = document.createElement("img");
+checkBtn.src ="check.png"
+taskSection.append(checkBtn)
+
+const taskTitle = document.createElement("span");
+taskTitle.innerText = "Go to gym"
+taskSection.append(taskTitle)
+
+
+const taskcross = document.createElement("img");
+taskcross.src = "cross.png"
+taskSection.append(taskcross)
+
+
